109897b69f858bc21e5f44318e7f70401f3ab490
outisa <savolainen.outi@gmail.com>
2020-08-08 16:54:40 +0300

    app for doccker exercise

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios'
 
-function App() {
+
+const App = () => {
+  const [joke, setJoke] = useState(null);
+  const getJoke = (event) => {
+    event.preventDefault();
+    axios.get('http://api.icndb.com/jokes/random?limitTo=[nerdy')
+      .then(response => {
+        console.log(response.data.value.joke)
+        setJoke(response.data.value.joke);
+      })
+  } 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Simple Chuck Norris -joke app</h2>
+      <p>Press the button to get a new nerdy Chuck Norris joke.</p>
+      {joke ? <p> {joke} </p> : null}
+      <button onClick={getJoke}>Press the button, get a joke!</button>
     </div>
   );
-}
+};
 
 export default App;
